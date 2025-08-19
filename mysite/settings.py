9bd5d4c07bfaef3109d0574d1abeffec20eafcca
E-Nincy <escotonincy10@outlook.es>
2025-08-19 13:52:30 +0800

    add more information

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig', # just 'polls' would work as well
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
